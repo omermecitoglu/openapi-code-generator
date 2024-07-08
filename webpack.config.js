b519542678c46f272aea33a7450fe928be9f6102
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-07-08 11:34:30 +0700

    fix output library type in webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
+    library: {
+      type: "commonjs2"
+    },
   },
   target: "node",
   plugins: [
