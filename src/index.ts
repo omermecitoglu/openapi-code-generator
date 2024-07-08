8dc71ec5d0e8a09a899e03ff4fc1b238e6d9d42f
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-07-08 16:32:50 +0700

    fix generateSchemaCode

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ import type { SchemaObject } from "@omer-x/openapi-types/schema";
 
 export function generateSchemaCode(name: string, schema: SchemaObject) {
   if (schema.type !== "object") throw new Error("Schema type is not object");
-  const properties = resolveProperties(schema.properties, schema.required ?? []);
-  const importedSchemas = resolveSchemasFromProps(schema.properties);
+  const properties = resolveProperties(schema.properties ?? {}, schema.required ?? []);
+  const importedSchemas = resolveSchemasFromProps(schema.properties ?? {});
   return generateSchema(name, properties, importedSchemas, schema.description);
 }
 
